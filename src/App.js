9191d2ee71af266118f2b6cff0a11699eab047d0
“Rafael Assis” <rafazassis@gmail.com>
2019-01-08 10:50:30 -0200

    Fragment blog initial state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactLogo from './ReactLogo';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <ReactLogo/>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
